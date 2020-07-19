b79224bbbb69fe7657c08e664fde55b954a2ce52
ELCHOCO <edgardojesus16@gmail.com>
2020-07-18 21:18:37 -0400

    worked on the lambda module

--- modules/compute/lambda/lambda.tf
+++ modules/compute/lambda/lambda.tf
@@ -1,13 +1,14 @@
 resource "aws_lambda_function" "lambda" {
   filename         = "${var.file-name}"
-  function_name    = "${function-name}"
+  function_name    = "${var.function-name}"
   role             = "${var.role}"
   handler          = "${var.handler}"
   source_code_hash = "${filebase64sha256("${var.file-name}")}"
-  runtime          = "${var.runtime}"
-  
+  #   source_code_hash = "${base64sha256(file("${var.file-name}"))}"
+  runtime = "${var.runtime}"
+
   tags = {
-    Name          = "${var.name}"
+    Name          = "${var.function-name}"
     Environment   = "${var.environment}"
     Template      = "${var.template}"
     Application   = "${var.application}"

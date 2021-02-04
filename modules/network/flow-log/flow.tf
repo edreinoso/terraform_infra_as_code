d2078164c551409d8da85f59037fd5cd30760398
ELCHOCO <edgardojesus16@gmail.com>
2021-02-04 12:17:38 +0100

    remodifying the vpc/flow-log modules

--- modules/network/flow-log/flow.tf
+++ modules/network/flow-log/flow.tf
@@ -1,18 +1,18 @@
 resource "aws_flow_log" "flow_logs" {
   iam_role_arn             = aws_iam_role.role.arn
   log_destination          = aws_cloudwatch_log_group.log_groups.arn
-  vpc_id                   = "${var.vpc-id}"
-  traffic_type             = "${var.traffic-type}"
-  max_aggregation_interval = "${var.max-aggregation-interval}"
+  vpc_id                   = var.vpc-id
+  traffic_type             = var.traffic-type
+  max_aggregation_interval = var.max-aggregation-interval
   tags                     = var.tags
 }
 
 resource "aws_cloudwatch_log_group" "log_groups" {
-  name = "${var.log-destination}"
+  name = var.log-destination
 }
 
 resource "aws_iam_role" "role" {
-  name = "${var.role-name}"
+  name = var.role-name
 
   assume_role_policy = <<EOF
 {
@@ -32,7 +32,7 @@ EOF
 }
 
 resource "aws_iam_role_policy" "role_policy" {
-  name = "${var.role-policy-name}"
+  name = var.role-policy-name
   role = aws_iam_role.role.id
 
   policy = <<EOF

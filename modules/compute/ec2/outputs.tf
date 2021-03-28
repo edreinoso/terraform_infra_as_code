e7d40eb680810d8e36b14f48acc7251f1b0e0442
ELCHOCO <edgardojesus16@gmail.com>
2021-03-28 13:46:40 +0200

    tiny change for outputing the ec2 public ip address

--- modules/compute/ec2/outputs.tf
+++ modules/compute/ec2/outputs.tf
@@ -4,6 +4,10 @@ output "ec2-id" {
     # value = "${aws_instance.ec2.*.id}"
 }
 
+output "ip" {
+  value = "${aws_instance.ec2.public_ip}"
+}
+
 output "public-dns" {
   value = "${aws_instance.ec2.public_dns}"
 }

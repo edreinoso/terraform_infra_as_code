0807a109d6b02242f557c178c19504b050fcb809
Edgardo Reinoso <e.reinoso@azerion.com>
2021-09-12 17:49:37 +0200

    modifying outputs from tg

--- modules/compute/load-balancer/tg/outputs.tf
+++ modules/compute/load-balancer/tg/outputs.tf
@@ -1,7 +1,7 @@
 output "target-arn" {
-  value = "${aws_lb_target_group.target-group.arn}"
+  value = aws_lb_target_group.target-group.arn
 }
 
 output "tg-arn-suffix" {
-  value = "${aws_lb_target_group.target-group.arn_suffix}"
+  value = aws_lb_target_group.target-group.arn_suffix
 }

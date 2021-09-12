074a52ae22e0082c8c15714e36f317c7c7dfa031
Edgardo Reinoso <e.reinoso@azerion.com>
2021-09-12 14:43:57 +0200

    making changes to the output of ec2 module

--- modules/compute/ec2/outputs.tf
+++ modules/compute/ec2/outputs.tf
@@ -1,13 +1,13 @@
 output "ec2-id" {
-  value = "${aws_instance.ec2.id}"
+  value = aws_instance.ec2.id
   # outputs for more than one ec2 instance
-    # value = "${aws_instance.ec2.*.id}"
+  # value = aws_instance.ec2.*.id
 }
 
 output "ip" {
-  value = "${aws_instance.ec2.public_ip}"
+  value = aws_instance.ec2.public_ip
 }
 
 output "public-dns" {
-  value = "${aws_instance.ec2.public_dns}"
+  value = aws_instance.ec2.public_dns
 }
